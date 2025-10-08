e5d5d95affc69d04dc16700430b1abe5dd5a138a
Ikawari-s <lnmanese@student.hau.edu.ph>
2025-10-08 13:45:22 +0800

    installed tailwidn

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
